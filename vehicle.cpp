bcde0aa4c9dae744e12162317b0c9446e01e43a1
siah.drews <siah.drews@gmail.com>
2024-05-03 17:15:42 -0600

    init vehicle.cpp

---
+++ vehicle.cpp
@@ -0,0 +1,22 @@
+// vehicle.cpp
+/*
+* Author: Josiah Andrews
+* Created: 5/3/2024
+* Description: 
+*/
+
+#include <iostream>
+
+// base class
+class Vehicle
+{
+    /******************** DATA MEMBERS************************/
+protected:
+    // protected members
+    
+private:
+    // private members
+
+public:
+    // public members
+};
