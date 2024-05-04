4b1bbd999211822352596976cac894ff1891cf57
siah.drews <siah.drews@gmail.com>
2024-05-03 18:38:53 -0600

    derived classes

--- vehicle.cpp
+++ vehicle.cpp
@@ -17,10 +17,7 @@ protected:
     std::string m_name;    // Name of vehicle
     int m_speed;           // current speed of vehicle
     bool m_moving = false; // is the vehicle moving or not
-
-private:
-    // private members
-    int m_max_speed; // maximum speed of vehicle
+        int m_max_speed; // maximum speed of vehicle
 
 public:
     bool still_running = true;
@@ -35,7 +32,6 @@ public:
 
     // TODO: Create methods that do something
 
-    virtual void accelerate() = 0;
     void accelerate() // speed up
     {
         if (m_moving == true)
@@ -43,21 +39,20 @@ public:
             if (m_speed + 50 <= m_max_speed)
             {
                 m_speed += 50;
-                std::cout << "Your " << m_name << " has sped up" << std::endl;
+                std::cout << "Your vehicle has sped up" << std::endl;
             }
             else
             {
                 m_speed = m_max_speed;
-                std::cout << "Your " << m_name << " is at maximum speed" << std::endl;
+                std::cout << "Your vehicle is at maximum speed" << std::endl;
             }
         } 
         else
         {
-           std::cout << "Your " << m_name << " needs to take off first" << std::endl; 
+           std::cout << "Your vehicle needs to take off first" << std::endl; 
         }
     }
 
-    virtual void decelerate() = 0;
     void decelerate() // slow down
     {
         if (m_moving == true)
@@ -65,44 +60,41 @@ public:
             if (m_speed - 50 <= 0)
             {
                 m_speed = 0;
-                std::cout << "Your " << m_name << " is stopped" << std::endl;
+                std::cout << "Your vehicle is stopped" << std::endl;
             }
             else
             {
                 m_speed -= 50;
-                std::cout << "Your " << m_name << " has slowed down" << std::endl;
+                std::cout << "Your vehicle has slowed down" << std::endl;
             }
         }
         else
         {
-            std::cout << "Your " << m_name << " needs to take off first" << std::endl;
+            std::cout << "Your vehicle needs to take off first" << std::endl;
         }
     }
 
-    virtual void takeoff() = 0;
     void takeoff() // start vehicle movement
     {
         if (m_moving == false){
             m_moving = true;
             m_speed = 50;
-            std::cout << "Your " << m_name << " has started moving at 50 speed" << std::endl;
+            std::cout << "Your vehicle has started moving at 50 speed" << std::endl;
         }
     }
 
-    virtual void stop_vehicle() = 0;
     void stop_vehicle() // stop vehicle
     {
         if (m_moving == true){
             m_moving = false;
             m_speed = 0;
-            std::cout << "Your " << m_name << " has stopped and is turned off" << std::endl;
+            std::cout << "Your vehicle has stopped and is turned off" << std::endl;
         }
     }
 
-    virtual void exit() = 0;
     void exit() // exit program
     {
-        std::cout << "Thanks for using your " << m_name << "! " << std::endl;
+        std::cout << "Thanks for using your vehicle! " << std::endl;
         still_running = false;
     }
 
@@ -113,12 +105,31 @@ public:
         std::cout << "+---------------------------------+" << std::endl;
     }
 
-    virtual char show_options() = 0;
-    char show_options(){
+    virtual void show_options() = 0;
+    void show_options(){
         char a;     // get input from user
         std::cout << "(T)ake off | (A)ccelerate | (D)ecelerate | (S)top | E(x)it >>";
         std::cin >> a;
-        return a;   // return input
+        
+        if(a=='t'){
+            takeoff();
+        } else if (a=='a')
+        {
+            accelerate();
+        } else if (a=='d')
+        {
+            decelerate();
+        } else if (a=='s')
+        {
+            stop_vehicle();
+        } else if (a=='x')
+        {
+            exit();
+        }
+        
+        
+        
+        
     }
 
     // TODO: Create getters and setters
@@ -150,5 +161,216 @@ public:
 };
 
 // TODO: Derived class 1
+class Boulder : public Vehicle{
+    public:
+    /*********************PUBLIC MEMBER FUNCTIONS*******************/
+    Boulder(std::string name, int speed, int max_speed) : Vehicle(name, speed, max_speed){}
+
+    // Create new method
+    void fall_off()
+    {
+        std::cout << "You fell off of the boulder and died" << std::endl;
+        still_running = false;
+    }
+
+    // override methods
+    void accelerate() // speed up
+    {
+        if (m_moving == true)
+        {
+            if (m_speed + 100 <= m_max_speed)
+            {
+                m_speed += 100;
+                std::cout << "Your boulder keeps going faster and faster" << std::endl;
+            }
+            else
+            {
+                m_speed = m_max_speed;
+                std::cout << "Your boulder is at terminal velocity" << std::endl;
+            }
+        } 
+        else
+        {
+           std::cout << "Your boulder can really only move if it's going downhill" << std::endl; 
+        }
+    }
+
+    void decelerate() // slow down
+    {
+        if (m_moving == true)
+        {
+            std::cout << "There isn't really much you can do to slow down your boulder" << std::endl;
+        }
+        else
+        {
+            std::cout << "your boulder needs a hill first" << std::endl;
+        }
+    }
+
+    void takeoff() // start vehicle movement
+    {
+        if (m_moving == false){
+            m_moving = true;
+            m_speed = 100;
+            std::cout << "You found a hill to roll your boulder down" << std::endl;
+        }
+    }
+
+    void stop_vehicle() // stop vehicle
+    {
+        if (m_moving == true){
+            m_moving = false;
+            m_speed = 0;
+            std::cout << "You got to the bottom of the hill and your boulder crashed" << std::endl;
+        }
+    }
+
+    void exit() // exit program
+    {
+        std::cout << "Thanks for using your boulder! " << std::endl;
+        still_running = false;
+    }
+
+    void print_title(){
+        std::cout << "+--------------------------------+" << std::endl;
+        std::cout << "|     Bob's Boulder Rolling      |"<< std::endl;
+        std::cout << "+--------------------------------+" << std::endl;
+    }
+
+    void show_options(){
+        char a;     // get input from user
+        std::cout << "(T)ake off | (A)ccelerate | (D)ecelerate | (S)top | E(x)it  | (F)all off >>";
+        std::cin >> a;
+        if(a=='t'){
+            takeoff();
+        } else if (a=='a')
+        {
+            accelerate();
+        } else if (a=='d')
+        {
+            decelerate();
+        } else if (a=='s')
+        {
+            stop_vehicle();
+        } else if (a=='x')
+        {
+            exit();
+        } else if (a=='f')
+        {
+            fall_off();
+        }
+    }
+
+};
 
 // TODO: Derived class 2
+class Airplane : public Vehicle{
+    public:
+    /*********************PUBLIC MEMBER FUNCTIONS*******************/
+    Airplane(std::string name, int speed, int max_speed) : Vehicle(name, speed, max_speed){}
+
+    // Create new method
+    void fall_off()
+    {
+        std::cout << "You fell out of the airplane and died" << std::endl;
+        still_running = false;
+    }
+
+    // override methods
+    void accelerate() // speed up
+    {
+        if (m_moving == true)
+        {
+            if (m_speed + 100 <= m_max_speed)
+            {
+                m_speed += 100;
+                std::cout << "Your Airplane sped up" << std::endl;
+            }
+            else
+            {
+                m_speed = m_max_speed;
+                std::cout << "Your Airplane is at maximum velocity" << std::endl;
+            }
+        } 
+        else
+        {
+           std::cout << "Your airplane needs to take off in order to speed up" << std::endl; 
+        }
+    }
+
+    void decelerate() // slow down
+    {
+        if (m_moving == true)
+        {
+            if (m_speed - 50 <= 0)
+            {
+                m_speed = 0;
+                std::cout << "Your Airplane is stopped" << std::endl;
+            }
+            else
+            {
+                m_speed -= 50;
+                std::cout << "Your Airplane has slowed down" << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "Your Airplane needs to take off first" << std::endl;
+        }
+    }
+
+    void takeoff() // start vehicle movement
+    {
+        if (m_moving == false){
+            m_moving = true;
+            m_speed = 100;
+            std::cout << "Your airplane took off into the sky" << std::endl;
+        }
+    }
+
+    void stop_vehicle() // stop vehicle
+    {
+        if (m_moving == true){
+            m_moving = false;
+            m_speed = 0;
+            std::cout << "Your airplane got to the airport and landed" << std::endl;
+        }
+    }
+
+    void exit() // exit program
+    {
+        std::cout << "Thanks for flying with us! " << std::endl;
+        still_running = false;
+    }
+
+    void print_title(){
+        std::cout << "+---------------------------------+" << std::endl;
+        std::cout << "|     Bob's Airplane Service      |"<< std::endl;
+        std::cout << "+---------------------------------+" << std::endl;
+    }
+
+    void show_options(){
+        char a;     // get input from user
+        std::cout << "(T)ake off | (A)ccelerate | (D)ecelerate | (S)top | E(x)it  | (F)all off >>";
+        std::cin >> a;
+        if(a=='t'){
+            takeoff();
+        } else if (a=='a')
+        {
+            accelerate();
+        } else if (a=='d')
+        {
+            decelerate();
+        } else if (a=='s')
+        {
+            stop_vehicle();
+        } else if (a=='x')
+        {
+            exit();
+        } else if (a=='f')
+        {
+            fall_off();
+        }
+    }
+
+};
